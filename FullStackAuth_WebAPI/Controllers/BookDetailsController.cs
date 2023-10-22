e6f66b3650aa4c34f926ed250b2f4eb60db78445
ChaseLoeffler <chaselo609@gmail.com>
2023-10-22 17:29:47 -0400

    Created Get request by book id in BookDetailsController and tested using Postman.

--- FullStackAuth_WebAPI/Controllers/BookDetailsController.cs
+++ FullStackAuth_WebAPI/Controllers/BookDetailsController.cs
@@ -1,5 +1,10 @@
 ﻿using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.DataTransferObjects;
+using FullStackAuth_WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -16,36 +21,49 @@ namespace FullStackAuth_WebAPI.Controllers
             _context = context;
         }
 
-        // GET: api/<BookDetailsController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
 
         // GET api/<BookDetailsController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{id}"),Authorize]
+        public IActionResult Get(string id)
         {
-            return "value";
+            try
+            {
+                string userId = User.FindFirstValue("id");
+                var favorites = _context.Favorites.Where(f => f.UserId.Equals(userId)).Where(f=>f.BookId.Equals(id));
+                bool favorited;
+                if (favorites.FirstOrDefault() != null)  {
+                    favorited = true;
+                }
+                else
+                {
+                    favorited = false;
+                }
+                var book = _context.Reviews.Select(b => new BookDetailsDto
+                {
+                    Reviews = _context.Reviews.Where(b => b.BookId.Equals(id)).Select(b => new ReviewWithUserDto
+                    {
+                        Id = b.Id,
+                        BookId = b.BookId,
+                        Text = b.Text,
+                        Rating = b.Rating,
+                        User = new UserForDisplayDto
+                        {
+                            Id = b.User.Id,
+                            FirstName = b.User.FirstName,
+                            LastName = b.User.LastName,
+                            UserName = b.User.UserName
+                        }
+                    }).ToList(),
+                    AverageRating = Math.Round(_context.Reviews.Select(b=>b.Rating).ToArray().Average(), 1),
+                    BookFavorited = favorited
+                }).ToList();
+                return StatusCode(200, book);
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
-        // POST api/<BookDetailsController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<BookDetailsController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<BookDetailsController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
--- FullStackAuth_WebAPI/DataTransferObjects/BookDetailsDto.cs
+++ FullStackAuth_WebAPI/DataTransferObjects/BookDetailsDto.cs
@@ -1,7 +1,11 @@
-﻿namespace FullStackAuth_WebAPI.DataTransferObjects
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FullStackAuth_WebAPI.DataTransferObjects
 {
     public class BookDetailsDto
     {
         public virtual ICollection<ReviewWithUserDto> Reviews { get; set; }
+        public double AverageRating { get; set; }
+        public bool BookFavorited { get; set; }
     }
 }
